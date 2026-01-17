ec3e7eabc30932c0278697674a676256e8914415
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-17 20:38:10 +0000

    Atualização

---
+++ contents/variaveis.py
@@ -0,0 +1,28 @@
+x = 5 #x recebe o valor 5
+y = 3.5
+z = 1+2j
+
+w = "Ana"
+w = 'Ana'
+
+#definindo várias variáveis ao mesmo tempo com valores DIFERENTES
+h, j, l = 1, 3, 5
+
+print(h)
+print(j)
+print(l)
+
+#definindo várias variáveis ao mesmo tempo com valores IGUAIS
+a = b = c = " faz o curso de Python3"
+
+print(a)
+print(b)
+print(c)
+
+#somatório com variáveis
+h = w + a
+
+print(h)
+
+l = x + j
+print(l)
