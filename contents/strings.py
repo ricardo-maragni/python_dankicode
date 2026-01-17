a990df23eb03e1f3e5ea2285759a5c97a5f52898
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-17 21:19:40 +0000

    Atualização

--- contents/strings.py
+++ contents/strings.py
@@ -28,3 +28,28 @@ e = "Ola"
 f = " Mundo"
 g = e + " " + f
 print(g)
+
+#Exercício 1
+
+Cliente = "Gabriel"
+Quantidade = "01"
+Livro = "Lógica de Programação"
+ValorLivro = "79,30"
+Vendedor = "Marcos"
+ValorComissao = "30,00"
+
+print(
+"Olá, " + Cliente + " sua compra de " + Quantidade + " qtd do livro " + Livro + " por R$ " + ValorLivro + " foi efetuada com sucesso!. "
+)
+
+print(
+"Olá, " + Vendedor + " você acaba de receber uma comissão de R$ " + ValorComissao + " pela compra realizada pelo(a) cliente " + Cliente
+)
+
+#Exercício 2
+
+Autora = "Beatriz"
+Titulo = "redes neurais"
+Compartilhamentos = "32"
+
+print("Olá, " + Autora + " seu post com o título " + Titulo + " gerou " + Compartilhamentos + " compartilhamentos.")
