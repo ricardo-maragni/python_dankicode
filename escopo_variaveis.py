d2b4a1debd8d5a4cbe523895cde867003ad82ff1
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-17 21:48:59 +0000

    Atualização

---
+++ escopo_variaveis.py
@@ -0,0 +1,26 @@
+"""escopo de variaveis"""
+
+"""
+as variaveis globais
+
+as variaveis locais
+"""
+
+x = 5
+
+def funcao():
+    x = 3
+    print("Valor da variavel local: ", x)
+
+funcao()
+print("Valor da variavel global: ", x)
+
+y = "Gabriel" #nome
+z = 1.74 #altura
+t = "000.000.000-00" #cpf
+l = 23 #idade
+
+nome = "Gabriel"
+altura = 1.74
+cpf = "000.000.000-00"
+idade = 23
