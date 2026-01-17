1e4c96131d1db67157550a261b8b10b7722f87d4
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-17 20:54:41 +0000

    Atualização

---
+++ contents/strings.py
@@ -0,0 +1,30 @@
+print("ola")
+print('ola')
+
+w = "Curso de Python3"
+print(w)
+
+"""comentário"""
+
+a = """ola,
+este eh o curso de python
+espero que goste"""
+
+print(a)
+
+#Métodos das Strings
+
+b = " Ola Mundo "
+print(b)
+print(b.strip())
+
+c = "Ola Mundo"
+print(c.lower())
+
+d = "ola mundo"
+print(d.upper())
+
+e = "Ola"
+f = " Mundo"
+g = e + " " + f
+print(g)
