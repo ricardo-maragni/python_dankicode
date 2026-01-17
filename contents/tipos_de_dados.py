67447652abcd7d5c857421de4deaf2b2188ce901
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-17 21:54:26 +0000

    Atualização

---
+++ contents/tipos_de_dados.py
@@ -0,0 +1,19 @@
+"""
+Variáveis e os Tipos de Dados
+
+Tipos de Dados:
+
+Tipos de Texto: str
+Tipos Numéricos: int, float e o complex
+
+Tipos de Sequência: list, tuple, range
+Tipos de Mapeamento: dict
+Tipos de Set: set, frozenset
+
+Tipo Booleano: bool
+Tipos binários: bytes, bytearray, memoryview
+
+print()
+type()
+#casting float, int, str
+"""
