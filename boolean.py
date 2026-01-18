143bb0a02e5dd05a73bc383fee3b40d4de822421
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-18 14:13:59 +0000

    Atualização

---
+++ boolean.py
@@ -0,0 +1,37 @@
+"""
+Operadores Lógicos e o Tipo Boolean
+
+Em ciência da computação, boolean, ou lógico,
+é um tipo de dado primitivo que possui dois valores,
+que podem ser considerados como (0 ou 1), falso ou verdadeiro.
+
+Chamado boolean em homenagem a George Boole
+"""
+
+# Tipo Boolean : True e False
+
+"""
+Operadores Lógicos
+
+and (e)
+or (ou)
+not (negação)
+"""
+
+"""
+Tabela Verdade
+
+Utilizando o "E"
+
+Verdadeiro e Verdadeiro - True and True = True
+Verdadeiro e Falso - True and False = False
+Falso e Verdadeiro - False and True = False
+Falso e Falso - False and False = False
+
+Utilizando o "OU"
+
+Verdadeiro ou Verdadeiro - True or True = True
+Verdadeiro ou Falso - True or False = True
+Falso ou Verdadeiro - False or True = True
+Falso ou Falso - False or False = False
+"""
