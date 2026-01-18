641c85266324cdde6975fd0e02152c30e2f9139e
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-18 13:44:25 +0000

    Atualização

---
+++ contents/modulo02_estruturas-logicas-e-condicionais/operadores.py
@@ -0,0 +1,26 @@
+'''
+Operadores Aritméticos
+'''
+
+nome_longo = 8
+x = 5
+y = 3
+
+# Operador de atribuição =
+
+"""
+Operador da soma/adição +
+Operador da subtração -
+Operador da multiplicação *
+Operador da divisão /
+
+Operador do resto %
+Operador da exponenciação **
+Operador da divisão inteira //
+"""
+
+x = 3
+y = 5
+z = y - x
+
+print(z)
