871ff8f26c77c5b582112a27e37fe9862a52bbb5
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-18 15:09:33 +0000

    Atualização

--- contents/modulo02_estruturas-logicas-e-condicionais/input.py
+++ contents/modulo02_estruturas-logicas-e-condicionais/input.py
@@ -1,5 +1,20 @@
+# Função input() - Função para receber dados usuário
+
+nome = input("Qual é o seu nome?")
+idade = input("Qual a sua idade?")
+
+print(f'Seu nome é: {nome} e sua idade é {idade}')
+
+# Outro jeito de fazer 1
 """
-Função input() - Função para receber dados usuário
+nome = input("Qual é o seu nome?")
+print("Olá,", nome)
+
+idade = input("Qual a sua idade?")
+print("Sua idade é:", idade)
 """
-print("Qual é o seu nome?")
-nome = input()
+
+# Outro jeito de fazer 2
+"""
+print("Seu nome é: {0} e sua idade é: {1}".format(nome, idade))
+"""
