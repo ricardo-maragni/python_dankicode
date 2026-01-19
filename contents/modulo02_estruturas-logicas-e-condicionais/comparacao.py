5298b330ba630bbde25ca9f9d8a33a73436914fa
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-19 13:32:36 +0000

    Atualização

---
+++ contents/modulo02_estruturas-logicas-e-condicionais/comparacao.py
@@ -0,0 +1,21 @@
+"""
+Operadores de Comparação
+
+Operador da Igualdade ==
+Operador da Diferença !=
+Operador maior que >
+Operador menor que <
+Operador maior que ou igual >=
+Operador menor que ou igual <=
+"""
+
+# Operador de atribuição =
+
+x = 3
+y = 5
+z = 3.0
+w = 8
+
+# x e z são iguais mesmo que x seja do tipo int e z seja do tipo float
+
+print(x == z)
