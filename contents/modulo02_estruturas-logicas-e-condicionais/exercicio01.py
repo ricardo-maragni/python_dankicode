28a46449b026d1bbe9f53f73cf9677b11e89ca59
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-21 14:00:29 +0000

    Atualização

---
+++ contents/modulo02_estruturas-logicas-e-condicionais/exercicio01.py
@@ -0,0 +1,11 @@
+# Exercício 01 - Área do Retângulo
+
+print("Calcule a área de um retângulo")
+
+base = input("Qual o tamanho da base do seu retângulo? ")
+
+altura = input("Qual o tamanho da altura do seu retângulo? ")
+
+area = float(base) * float(altura)
+
+print(f"O seu retângulo possui área: {area} unidade de medida")
---
+++ contents/modulo02_estruturas-logicas-e-condicionais/exercicios-descricao.py
@@ -0,0 +1,14 @@
+"""
+Exercícios - Python
+
+Observação: Todos os exercícios devem utilizar a função input,
+de forma que o usuário possa determinar os dados de entrada.
+
+01 - área de um retângulo
+02 - área de um quadrado
+04 - se o produto que você quer avaliar custa (??) reais qual
+será o valor do mesmo com desconto de (??)%
+05 - área de um círculo (pi = 3.141592)
+06 - conversão de reais para dolar
+07 - conversão de dolar para reais
+"""
