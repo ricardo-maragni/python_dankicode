a8e0153d0c6eb81e0fc9272b8f14965b5151ba37
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-21 17:53:15 +0000

    Atualização

--- contents/modulo02_estruturas-logicas-e-condicionais/exercicio-descricao.py
+++ contents/modulo02_estruturas-logicas-e-condicionais/exercicio-descricao.py
@@ -8,7 +8,7 @@ de forma que o usuário possa determinar os dados de entrada.
 02 - área de um quadrado
 03 - se o produto que você quer avaliar custa (??) reais qual
 será o valor do mesmo com desconto de (??)%
-04 - área de um círculo (pi = 3.141592)
+04 - área de um círculo (pi = 3.14159265359)
 05 - conversão de reais para dolar
 06 - conversão de dolar para reais
 """
---
+++ contents/modulo02_estruturas-logicas-e-condicionais/exercicio04.py
@@ -0,0 +1,11 @@
+# Calcular a área de um círculo
+
+print("Calcule a área de um círculo")
+
+pi = 3.14159265359
+
+raio = input("Qual é o tamanho do raio (distância do centro até a borda)? ")
+
+area = float(pi) * (float(raio) * float(raio))
+
+print(f"A área do círculo é: {area}")
