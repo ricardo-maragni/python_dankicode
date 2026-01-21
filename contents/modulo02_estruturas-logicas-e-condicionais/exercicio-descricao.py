a4b3666c5709ef47b3d9e4dbfe756c47fb6896a2
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-21 14:04:54 +0000

    Atualização

--- contents/modulo02_estruturas-logicas-e-condicionais/exercicio-descricao.py
+++ contents/modulo02_estruturas-logicas-e-condicionais/exercicio-descricao.py
@@ -6,9 +6,9 @@ de forma que o usuário possa determinar os dados de entrada.
 
 01 - área de um retângulo
 02 - área de um quadrado
-04 - se o produto que você quer avaliar custa (??) reais qual
+03 - se o produto que você quer avaliar custa (??) reais qual
 será o valor do mesmo com desconto de (??)%
-05 - área de um círculo (pi = 3.141592)
-06 - conversão de reais para dolar
-07 - conversão de dolar para reais
+04 - área de um círculo (pi = 3.141592)
+05 - conversão de reais para dolar
+06 - conversão de dolar para reais
 """
---
+++ contents/modulo02_estruturas-logicas-e-condicionais/exercicio02.py
@@ -0,0 +1,9 @@
+# Área de um quadrado
+
+print("Calcule a área de um quadrado")
+
+comprimento = input("Qual é o comprimento de um lado do seu quadrado? ")
+
+area = float(comprimento) * float(comprimento)
+
+print(f"O seu quadrado possui área: {area} unidade de medida")
