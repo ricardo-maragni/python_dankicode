47566ce9ba67699a9bf05fe96978915ab4bd9994
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-27 14:46:36 +0000

    Atualização

--- contents/modulo03_estruturas-de-repeticao/while.py
+++ contents/modulo03_estruturas-de-repeticao/while.py
@@ -1,1 +1,19 @@
-# Continuar
+"""
+Estruturas de repetição - loops
+
+while
+for
+do while
+"""
+
+# Valor inicial
+a = 0
+print(f"Valor inicial: {a}")
+
+# Acrescentar +1 até chegar em 5
+while a <= 5:
+    print(a)
+    a = a + 1
+
+# Resultado final
+print(f"Resultado final: {a}")
