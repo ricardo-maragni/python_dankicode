13f34ce439f5240845a48790fe328216eaa8e22c
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-28 14:48:15 +0000

    Atualização

---
+++ contents/modulo03_estruturas-de-repeticao/verificando-sinal-numerico.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite o número: "))
+
+if numero > 0:
+    print(f"O {numero} é positivo")
+elif numero == 0:
+    print(f"O {numero} é igual a zero")
+else:
+    print(f"O {numero} é menor que zero")
