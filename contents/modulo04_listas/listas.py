cf1e25f1d7f4d335b193ade5bd732d90651adb9e
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-01-29 21:00:12 +0000

    Atualização

---
+++ contents/modulo04_listas/listas.py
@@ -0,0 +1,29 @@
+print("-" * 99)
+
+# Lista contendo apenas strings (textos)
+lista = ["chicago", "queens", "salvador", "pernambuco"]
+print(lista)
+
+# Lista contendo apenas números inteiros
+lista2 = [2, 3, 7, 8, 10]
+print(lista2)
+
+# Lista contendo números decimais (float)
+lista3 = [2.0, 3.5, 6.3]
+print(lista3)
+
+# Lista contendo valores booleanos
+lista4 = [True, False]
+print(lista4)
+
+# Lista mista: pode conter diferentes tipos de dados
+lista5 = [True, "chicago", 2.5, False, 4]
+print(lista5)
+
+# Mostra o tipo da variável lista5 (list)
+print(type(lista5))
+
+# Acessa o elemento da posição 1 da lista (índice começa em 0)
+print(lista5[1])
+
+print("-" * 99)
