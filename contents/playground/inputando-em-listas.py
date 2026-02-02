c8809c535395bf709ab24c031ca9aa8736f58388
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-02 19:54:12 +0000

    Atualização

--- contents/playground/inputando-em-listas.py
+++
@@ -1,5 +0,0 @@
-# Inicia o código
-print("-" * 99)
-
-# Finaliza o código
-print("-" * 99)
---
+++ contents/playground/supermercado-python.py
@@ -0,0 +1,18 @@
+# Inicia o código
+print("-" * 99)
+
+print("Bem vindo(a) ao Supermercado Python!")
+
+carrinho_de_compras = []
+
+usuario = input("Qual é o seu nome? ")
+
+qtd_itens = int(input(f"Ok {usuario}, quantos itens gostaria de comprar? "))
+
+for x in range(qtd_itens):
+    carrinho_de_compras.append(input("Digite aqui o produto que gostaria de comprar: "))
+
+print(f"SENSACIONAL. Os itens comprados foram: {carrinho_de_compras}")
+
+# Finaliza o código
+print("-" * 99)
