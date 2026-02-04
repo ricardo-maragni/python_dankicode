32c28a0c97173c3da086485d39731356cfe7f469
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-04 15:22:40 +0000

    Atualização

--- contents/modulo06_dicionarios/dicionarios.py
+++ contents/modulo06_dicionarios/dicionarios.py
@@ -34,3 +34,31 @@ if "idade" in dicionario2:
     print("A idade existe")
 
 print(dicionario2.items())
+
+# Modificar itens de um dicionário
+
+print(dicionario2["nome"])
+dicionario2["nome"] = "Pedro"
+print(dicionario2["nome"])
+
+# Adicionar chave
+
+print(dicionario2)
+dicionario2.update({"estado":"Rio de Janeiro"})
+print(dicionario2)
+
+
+# Remover chave
+
+dicionario2.popitem() # remove a última chave
+
+dicionario2.pop("idade") # remove uma chave específica
+
+del dicionario2["nacionalidade"] # remover uma chave específica
+
+print(dicionario2)
+
+# Limpa o dicionário
+
+dicionario2.clear()
+print(dicionario2)
