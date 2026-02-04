c8f4f47c03c237c748cfe595ce28578b055d2e3f
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-04 18:47:39 +0000

    Atualização

---
+++ contents/modulo07_sets/sets.py
@@ -0,0 +1,13 @@
+"""
+Listas: Coleção ordenada, mutável e que permite valores duplicados
+Tuplas: Coleção ordenada, imutável e que permite valores duplicados
+Dicionarios: Coleção não ordenada, mutável e que não permite valores duplicados
+Sets: Coleção não ordenada, imutável e que não permite valores duplicados
+"""
+
+# Os sets também são conhecidos como conjuntos
+
+conjunto = {"item1", "item2", "item3"}
+print(type(conjunto))
+print(conjunto)
+print(len(conjunto))
