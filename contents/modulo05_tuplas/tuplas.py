edaa0beec17fba1386dfc6d1e8acec3689ed1352
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-04 12:35:36 +0000

    Atualização

--- contents/modulo05_tuplas/tuplas.py
+++ contents/modulo05_tuplas/tuplas.py
@@ -1,1 +1,24 @@
-# Continuar
+# Início
+print("-"*99)
+
+# Exemplo de lista
+
+print("Lista")
+lista = ["item1", "item2", "item3"]
+print(lista)
+print(len(lista))
+print(type(lista))
+print(dir(lista))
+
+# Exemplo de tupla
+
+print("")
+print("Tupla")
+tupla = ("item1", "item2", "item3")
+print(tupla)
+print(len(tupla))
+print(type(tupla))
+print(dir(tupla)) # Verifica os atributos do objeto
+
+# Fim
+print("-"*99)
