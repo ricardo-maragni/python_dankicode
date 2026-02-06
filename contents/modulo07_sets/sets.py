d76e7cc36213be7e830926614e3774687f3f6e45
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-06 17:47:42 +0000

    Atualização

--- contents/modulo07_sets/sets.py
+++ contents/modulo07_sets/sets.py
@@ -11,3 +11,25 @@ conjunto = {"item1", "item2", "item3"}
 print(type(conjunto))
 print(conjunto)
 print(len(conjunto))
+
+# Adicionar um item ao conjunto
+
+conjunto.add("item4")
+print(conjunto)
+
+# Adicionar vários itens ao conjunto
+
+novos_itens = {"item5", "item6", "item7", "item8", "item9"}
+conjunto.update(novos_itens)
+print(conjunto)
+
+# Remover elementos
+
+conjunto.remove("item4")
+conjunto.discard("item5")
+print(conjunto)
+
+# Limpar o conjunto
+
+conjunto.clear()
+print(conjunto)
