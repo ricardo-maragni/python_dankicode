6828366149209e73813bba747db649f025a1d357
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-06 18:39:47 +0000

    Atualização

---
+++ contents/modulo08_funcoes/funcoes.py
@@ -0,0 +1,19 @@
+# Paradigma Imperativo
+
+# Bloco externo
+
+nome = "Gabriel" # Variável global
+
+# Define a função
+def minha_funcao():
+    # Bloco interno *bloco interno de uma função é conhecido como corpo de uma função
+    nome = "Ana" # Variável local
+    print(nome)
+
+
+# Sempre é preciso pular 2 linhas depois de uma função para evitar erros
+
+# Roda a função
+minha_funcao()
+
+print(nome) 
