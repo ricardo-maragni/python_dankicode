3139804c54affb62e74926b975f75a1f3f9ca2de
Ricardo Augusto Maragni Gomes da Silva <114947208+ricardo-maragni@users.noreply.github.com>
2026-02-06 18:06:22 +0000

    Atualização

--- contents/modulo07_sets/sets.py
+++ contents/modulo07_sets/sets.py
@@ -7,29 +7,45 @@ Sets: Coleção não ordenada, imutável e que não permite valores duplicados
 
 # Os sets também são conhecidos como conjuntos
 
-conjunto = {"item1", "item2", "item3"}
-print(type(conjunto))
-print(conjunto)
-print(len(conjunto))
+conjunto1 = {"item1", "item2", "item3"}
+print(type(conjunto1))
+print(conjunto1)
+print(len(conjunto1))
 
-# Adicionar um item ao conjunto
+# Adicionar um item ao conjunto1
 
-conjunto.add("item4")
-print(conjunto)
+conjunto1.add("item4")
+print(conjunto1)
 
 # Adicionar vários itens ao conjunto
 
-novos_itens = {"item5", "item6", "item7", "item8", "item9"}
-conjunto.update(novos_itens)
-print(conjunto)
+pacote1 = {"item5", "item6", "item7", "item8", "item9"}
+conjunto1.update(pacote1)
+
+print(conjunto1)
 
 # Remover elementos
 
-conjunto.remove("item4")
-conjunto.discard("item5")
-print(conjunto)
+conjunto1.remove("item4")
+conjunto1.discard("item5")
+print(conjunto1)
 
 # Limpar o conjunto
 
-conjunto.clear()
-print(conjunto)
+conjunto1.clear()
+print(conjunto1)
+
+# Unir conjuntos
+
+conjunto1 = {"item1", "item2", "item3"}
+conjunto2 = {"item10", "item11", "item12", "item13", "item14", "item15"}
+conjunto3 = conjunto1.union(conjunto2)
+print(conjunto3)
+
+# Interseção de conjuntos
+
+conjunto4 = {1, 2, 3}
+conjunto5 = {3, 4, 5}
+
+conjunto6 = conjunto4.intersection(conjunto5)
+print(conjunto6)
